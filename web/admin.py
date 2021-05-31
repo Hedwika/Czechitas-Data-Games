30d5dde55c71b611c1ef1544e270e16c9ae897ad
hedwika <pajerova.hedvika@gmail.com>
2021-05-31 19:18:00 +0200

    Issue #2: Model "Event" created

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+admin.site.register(models.Event)
--- web/models.py
+++ web/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    description = models.CharField(max_length=1000)
