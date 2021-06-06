7e94725ac503f84394a759130086c5e40e996458
katroska <uzkodime0221>
2021-06-06 14:57:00 +0200

    issue #3: model Assignment created

--- web/models.py
+++ web/models.py
@@ -4,3 +4,9 @@ class Event(models.Model):
     start = models.DateTimeField()
     end = models.DateTimeField()
     description = models.CharField(max_length=1000)
+
+class Assignment(models.Model):
+    description = models.CharField(max_length=1000)
+    data = models.FileField()
+    right_answer = models.CharField(max_length=200)
+    order = models.IntegerField()
