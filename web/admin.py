364da842d6143c5d14ce0a22904d072c30f8e747
hedwika <pajerova.hedvika@gmail.com>
2021-08-05 21:08:39 +0200

    Assignment view created, event and assignment connected

--- web/admin.py
+++ web/admin.py
@@ -3,3 +3,4 @@ from django.contrib import admin
 from . import models
 admin.site.register(models.Event)
 admin.site.register(models.Assignment)
+admin.site.register(models.NewUser)
---
+++ web/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from web import models
+from web.models import Assignment
+
+
+class RightAnswer(forms.Form):
+    answer = forms.CharField(max_length=200)
+
+    def __init__(self, *args, **kwargs):
+        self.right_answer = kwargs.pop("right_answer")
+        super().__init__(*args, **kwargs)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        if cleaned_data["answer"] != self.right_answer:
+            raise forms.ValidationError("Špatná odpověď, zkus to prosím znovu.")
+        return cleaned_data
