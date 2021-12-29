7b816830eba130a9f522637eb9b443dd7fc0ff3d
Jiří Pešík <jiri.pesik@outlook.cz>
2021-12-29 23:01:52 +0100

    Right answer timestamp (#53)
    * Right answer timestamp
    
    * Answer timestamp added
    
    * Answer timestamp added
    
    * Migration fix
    
    * Answers validation
    
    * Rename answer field
    
    Co-authored-by: hedwika <pajerova.hedvika@gmail.com>
    Co-authored-by: Jiří Pešík <jiri.pesik@gmail.com>

--- web/forms.py
+++ web/forms.py
@@ -1,4 +1,5 @@
 import math
+from datetime import datetime
 
 from django import forms
 from web import models
@@ -9,55 +10,35 @@ class RightAnswer(forms.Form):
     answer = forms.CharField(max_length=200, label="",
                              widget=forms.TextInput(attrs={"class": "form-control"}))
 
-    # TODO: Move this to a method
-    # if Assignment.answer_type == 'SEZNAM':
-    #     answer_form = answer_form.join(answer_form.split())
-    #     answer_form = [answer_form]
-    #     answer_form = answer_form.sort()
-    #
-    # elif Assignment.answer_type == 'ČÍSLO':
-    #     def round_half_up(n, decimals=0):
-    #         multiplier = 10 ** decimals
-    #         return math.floor(n * multiplier + 0.5) / multiplier
-    #     answer_form = round_half_up(answer_form , 2)
-    #
-    # else:
-    #     answer_form = answer_form.lower()
-    #     answer_form = answer_form.join(answer_form.split())
+    assignment: models.Assignment
 
     def __init__(self, *args, **kwargs):
-        self.right_answer = kwargs.pop("right_answer")
+        self.assignment = kwargs.pop("assignment")
         super().__init__(*args, **kwargs)
 
     def clean(self):
         cleaned_data = super().clean()
-        answer = cleaned_data["answer"]
-
-        if answer != self.right_answer:
-
-            if Assignment.answer_type == 'SEZNAM':
-                answer.split()
-                answer.sort()
-
-                if self.right_answer.__hash__() != answer.__hash__():
-                    raise forms.ValidationError("Špatná odpověď, zkus to prosím znovu.")
-
-            elif Assignment.answer_type == 'ČÍSLO':
-
-                def round_half_up(n, decimals=0):
-                    multiplier = 10 ** decimals
-                    return math.floor(n * multiplier + 0.5) / multiplier
-
-                answer = round_half_up(answer, 2)
-
-                if self.right_answer != answer:
-                    raise forms.ValidationError("Špatná odpověď, zkus to prosím znovu.")
-
-            elif Assignment.answer_type == 'TEXT':
-                answer.lower()
-
-                if self.right_answer != answer:
-                    raise forms.ValidationError("Špatná odpověď, zkus to prosím znovu.")
-
-            raise forms.ValidationError("Špatná odpověď, zkus to prosím znovu.")
+        if self.assignment.answer_type == 'SEZNAM':
+            right_answer = list(map(lambda x: x.strip(), self.assignment.right_answer.split(",")))
+            answer = cleaned_data["answer"]
+            if "," not in answer:
+                raise forms.ValidationError("Odpověď musí být seznam a musí obsahovat alespoň dvě "
+                                            "hodnoty oddělené čárkou.")
+            answer = list(map(lambda x: x.strip(), answer.split(",")))
+            if not set(right_answer) == set(answer):
+                raise forms.ValidationError("Špatná odpověď, zkus to prosím znovu.")
+        elif self.assignment.answer_type == 'ČÍSLO':
+            right_answer = float(self.assignment.right_answer)
+            answer = cleaned_data["answer"]
+            if "," in answer:
+                raise forms.ValidationError("Je třeba používat desetinnou tečku, nikoli desetinnou čárku.")
+            elif not answer.replace('.', '', 1).isdigit():
+                raise forms.ValidationError("Odpověď musí být číslo!")
+            else:
+                answer = float(cleaned_data["answer"])
+            if round(answer, 2) != round(right_answer, 2):
+                raise forms.ValidationError("Špatná odpověď, zkus to prosím znovu.")
+        else:
+            if cleaned_data["answer"] != self.assignment.right_answer:
+                raise forms.ValidationError("Špatná odpověď, zkus to prosím znovu.")
         return cleaned_data
